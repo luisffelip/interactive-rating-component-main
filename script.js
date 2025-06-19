ef3908441b8778ba733af7b9481888773d81dd91
luisffelip <lfbarbosaaz@gmail.com>
2025-06-19 17:28:16 -0300

    Começando o JS (Verificação se o usuário selecionou alguma nota)

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-const nota = document.getElementsByTagName('label');
-const btn = document.getElementsByTagName('button');
+const form = document.getElementById('form-rate')
+
+form.addEventListener('submit', function(e){
+    const rankSelected = document.querySelector('input[name="nota"]:checked');
+
+    if (!rankSelected){
+        e.preventDefault();
+        alert('Por favor, selecione uma nota');
+    };
+
+    // const paragraph = document.createElement('p');
+    // paragraph.id = 'rank';
+    // paragraph.textContent = `Você selecionou ${rankSelected} de 5.`
+});
+
